7865566deea60b06cede762504b023350b4b7159
saurako <saurako@simplymeasured.com>
2018-01-10 20:27:51 -0800

    Allow non-email userids

--- web/app/view/dialog/Register.js
+++ web/app/view/dialog/Register.js
@@ -41,7 +41,7 @@ Ext.define('Traccar.view.dialog.Register', {
             name: 'email',
             fieldLabel: Strings.userEmail,
             validator: function (val) {
-                if (/(.+)@(.+)\.(.{2,})/.test(val)) {
+                if (/([^\s\\]+)/.test(val)) {
                     return true;
                 } else {
                     return Ext.form.field.VTypes.emailText;
